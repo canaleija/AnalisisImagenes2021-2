093b992506fc88329f57355059692e295b6a4687
working <working@DESKTOP-17KGHMK>
2021-03-05 10:23:11 -0600

    05/03/21 - Lectura de  la imagen

--- src/analisisimagenes2021/pkg2/AnalisisImagenes20212.java
+++ src/analisisimagenes2021/pkg2/AnalisisImagenes20212.java
@@ -5,6 +5,10 @@
  */
 package analisisimagenes2021.pkg2;
 
+import java.awt.Color;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+
 /**
  *
  * @author working
@@ -15,7 +19,16 @@ public class AnalisisImagenes20212 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       // visualización de la imagen dentro de la GUI
+        Image imagen = herramientas.HerramientasImagen.abrirImagen();
+        // para cuantización de la imagen vamos a un ocupar BufferedImage
+        BufferedImage bImagen = herramientas.HerramientasImagen.toBufferedImage(imagen);
+        
+        Color color = new Color(bImagen.getRGB(300,240));
+        int r = color.getRed();
+        int g =color.getGreen();
+        int b = color.getBlue();
+        System.out.println();
     }
     
 }
